8d829c71c2d0e20b4400333b9f11e468bc33c86b
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2023-11-18 14:50:15 -0500

    Create FluxAndMonoGeneratorService

--- reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
+++ reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
@@ -1,4 +1,21 @@
 package com.learnreactiveprogramming.service;
 
+import reactor.core.publisher.Flux;
+
+import java.util.List;
+
 public class FluxAndMonoGeneratorService {
+    public static void main(String[] args) {
+        new FluxAndMonoGeneratorService()
+                .namesFlux()
+                // access the flux
+                .subscribe(name -> {
+                    System.out.println("Name is :" + name);
+                });
+    }
+
+    public Flux<String> namesFlux() {
+        // simulate db or remote data source
+        return Flux.fromIterable(List.of("Alex", "Ben", "Chloe"));
+    }
 }
