1a89c8f20531d2151c7b05dac98ba4a8f073ec5a
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2023-11-18 14:57:10 -0500

    mono name

--- reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
+++ reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
@@ -1,6 +1,7 @@
 package com.learnreactiveprogramming.service;
 
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.util.List;
 
@@ -10,12 +11,22 @@ public class FluxAndMonoGeneratorService {
                 .namesFlux()
                 // access the flux
                 .subscribe(name -> {
-                    System.out.println("Name is :" + name);
+                    System.out.println("Flux Name is :" + name);
+                });
+
+        new FluxAndMonoGeneratorService()
+                .nameMono()
+                .subscribe(name -> {
+                    System.out.println("Mono Name is " + name);
                 });
     }
 
     public Flux<String> namesFlux() {
         // simulate db or remote data source
         return Flux.fromIterable(List.of("Alex", "Ben", "Chloe"));
+    }
+
+    public Mono<String> nameMono() {
+        return Mono.just("alex");
     }
 }
