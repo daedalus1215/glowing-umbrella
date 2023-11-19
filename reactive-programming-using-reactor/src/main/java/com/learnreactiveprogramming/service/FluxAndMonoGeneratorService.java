5f932f9e6110d5a71895ec2be64766ee95beac82
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2023-11-18 23:00:08 -0500

    more movies controller test

--- reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
+++ reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
@@ -1,39 +1,4 @@
 package com.learnreactiveprogramming.service;
 
-import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
-
-import java.util.List;
-
 public class FluxAndMonoGeneratorService {
-    public static void main(String[] args) {
-        new FluxAndMonoGeneratorService()
-                .namesFlux()
-                // access the flux
-                .subscribe(name -> {
-                    System.out.println("Flux Name is :" + name);
-                });
-
-        new FluxAndMonoGeneratorService()
-                .nameMono()
-                .subscribe(name -> {
-                    System.out.println("Mono Name is " + name);
-                });
-    }
-
-    public Flux<String> namesFlux() {
-        // simulate db or remote data source
-        return Flux.fromIterable(List.of("Alex", "Ben", "Chloe")).log();
-    }
-
-    public Flux<String> namesFluxTransformation() {
-        // simulate db or remote data source
-        return Flux.fromIterable(List.of("alex", "ben", "chloe"))
-                .map(String::toUpperCase)
-                .log();
-    }
-
-    public Mono<String> nameMono() {
-        return Mono.just("alex");
-    }
 }
--- reactive-programming-using-reactor/src/test/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorServiceTest.java
+++ reactive-programming-using-reactor/src/test/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorServiceTest.java
@@ -1,44 +1,7 @@
 package com.learnreactiveprogramming.service;
 
-import org.junit.jupiter.api.Test;
-import reactor.core.publisher.Flux;
-import reactor.test.StepVerifier;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class FluxAndMonoGeneratorServiceTest {
-    FluxAndMonoGeneratorService target = new FluxAndMonoGeneratorService();
 
-    @Test
-    void namesFlux_noArgument_shouldReturnExpected() {
-        // Act
-        Flux<String> actual = target.namesFlux();
-
-        // Assert
-        StepVerifier.create(actual)
-                .expectNext("Alex", "Ben", "Chloe")
-                .verifyComplete();
-    }
-
-    @Test
-    void namesFlux_noArgument_shouldReturnExpectedCount() {
-        // Arrange & Act
-        Flux<String> actual = target.namesFlux();
-
-        // Assert
-        StepVerifier.create(actual)
-                .expectNextCount(3)
-                .verifyComplete();
-    }
-
-    @Test
-    void namesFluxTransformation_noArgument_shouldReturnExpected() {
-        // Arrange & Act
-        Flux<String> actual = target.namesFluxTransformation();
-
-        // Assert
-        StepVerifier.create(actual)
-                .expectNext("ALEX", "BEN", "CHLOE")
-                .verifyComplete();
-    }
 }
